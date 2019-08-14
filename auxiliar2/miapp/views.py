9e09beb297beb7dd61aebc95377dff888d69d432
Sebarichpoint <sebi.pg@gmail.com>
2019-08-13 22:58:12 -0400

    proyecto desafío 1

---
+++ auxiliar2/miapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'miapp/index.html')
+
+def pestaña(request):
+	return render(request, 'miapp/pestaña.html')
+
+def edocente(request):
+	return render(request, 'miapp/edocente.html')
